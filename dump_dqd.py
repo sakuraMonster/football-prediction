048b422557c78aedca46754bea87ff2cc3abe82a
zhangxuejun <zhangxuejun@hd123.com>
2026-05-09 16:13:32 +0800

    删除多余的问题

--- dump_dqd.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'https://www.dongqiudi.com/team/50000512.html'
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'}
-res = requests.get(url, headers=headers)
-with open('dqd_team.html', 'w', encoding='utf-8') as f:
-    f.write(res.text)
